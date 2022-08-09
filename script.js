0f7af22f1fb94a6db8f192870ab13913deea0222
Mahmoodulhasan Bhaiyat <w.alutrujje@gmail.com>
2022-08-08 20:26:39 -0400

    Fix problems game work better but code messy

--- script.js
+++ script.js
@@ -3,55 +3,67 @@
 // make a function that plays a round of rps and returns the score
 // make a function for the game and within that make a function that plays 5 rounds
 
-let weapons = [
-    "pink slipper", // rock
-    "coffee mug", // paper
-    "joana" // scissor
-  ];
- 
-let computerChoice = weapons[Math.floor(Math.random()*weapons.length)]; 
-console.log(computerChoice);
 
-let playerChoice = prompt("choose yer damn weapon: pink slipper, coffee mug, joana.","pink slipper"); //weapons[Math.floor(Math.random()*weapons.length)]; prompt("choose yer damn weapon: pink slipper, coffee mug, joana.","pink slipper")
-console.log(playerChoice);
 
-let playerScore = 0;
-let computerScore = 0;
 
-function rpsRound (playerChoice, computerChoice) {
-    if      (playerChoice === "pink slipper" && computerChoice === "coffee mug"
-            || playerChoice === "coffee mug" && computerChoice === "joana" 
-            || playerChoice === "joana" && computerChoice === "pink slipper") {
-            
-                computerScore++;
-                console.log(computerScore);
-                return "you lost, nyany nyany boo boo";
-    }
-    else if (playerChoice === "pink slipper" && computerChoice === "joana"
-            || playerChoice === "coffee mug" && computerChoice === "pink slipper" 
-            || playerChoice === "joana" && computerChoice === "coffee mug") {
-            
-                playerScore++;
-                console.log(playerScore);
-                return "you won! poor joana";
-                    
-    }
-    else if (playerChoice === "pink slipper" && computerChoice === "pink slipper"
-            || playerChoice === "coffee mug" && computerChoice === "coffee mug" 
-            || playerChoice === "joana" && computerChoice === "joana") {
-        
-                return "tied";
-    }
-}
 
 function game () {
-    for (let i = 0; i < 5; i++) {
-    console.log(rpsRound(playerChoice,computerChoice));
+    
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function rpsRound (a, b) {
+
+        let weapons = [
+            "pink slipper", // rock
+            "coffee mug", // paper
+            "joana" // scissor
+          ];
+         
+        let computerChoice = weapons[Math.floor(Math.random()*weapons.length)]; 
+        console.log(`slamBot: ${computerChoice}`);
+        
+        let playerChoice = prompt("choose yer damn weapon: pink slipper, coffee mug, joana.","pink slipper"); //weapons[Math.floor(Math.random()*weapons.length)]; prompt("choose yer damn weapon: pink slipper, coffee mug, joana.","pink slipper")
+        console.log(`slamBert: ${playerChoice}`);
+
+        if      (playerChoice === "pink slipper" && computerChoice === "coffee mug"
+                || playerChoice === "coffee mug" && computerChoice === "joana" 
+                || playerChoice === "joana" && computerChoice === "pink slipper") {
+                
+                    computerScore++;
+                    console.log(`slamBot has ${computerScore} points`);
+                    console.log(`slamBert has ${playerScore} points`);
+                    return "you lost, nyany nyany boo boo";
+        }
+        else if (playerChoice === "pink slipper" && computerChoice === "joana"
+                || playerChoice === "coffee mug" && computerChoice === "pink slipper" 
+                || playerChoice === "joana" && computerChoice === "coffee mug") {
+                
+                    playerScore++;
+                    console.log(`slamBot has ${computerScore} points`);
+                    console.log(`slamBert has ${playerScore} points`);
+                    return "you won! poor joana";
+                        
+        }
+        else if (playerChoice === "pink slipper" && computerChoice === "pink slipper"
+                || playerChoice === "coffee mug" && computerChoice === "coffee mug" 
+                || playerChoice === "joana" && computerChoice === "joana") {
+            
+                    console.log(`slamBot has ${computerScore} points`);
+                    console.log(`slamBert has ${playerScore} points`);
+                    return "tied";
+        }
     }
-    if (playerScore >= 3) {
+
+    for (let i = 0; i < 5; i++) {
+    console.log(rpsRound());
+    }
+    if (playerScore > computerScore) {
         console.log("yaay, you won the game!");
-    } else {
-        console.log("boo, you lost to a computer!");
+    } else if (playerScore < computerScore) {
+        console.log("pfft, you lost to slamBot;)");
+    } else if (playerScore === computerScore) {
+        console.log("huh, it's a tie");
     }
 }
 
